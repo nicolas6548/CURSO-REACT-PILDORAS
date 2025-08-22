4ed3b7f40cdcacb4618cf7f0e3f216186d5b74bd
Nicolas Peralta <nicolasperalta6548@gmail.com>
2025-08-22 19:01:48 -0300

    viendo si me deja subir los archivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function GaleriaDeImagenes()
+{
+  const [imagenes, setImagenes] = useState([
+    {
+      src: "img/paisaje-1.jpg",
+      alt: "Imagen 1",
+      caption: "Pie de foto Imagen 1",
+    },
+    {
+      src: "img/paisaje-2.jpg",
+      alt: "Imagen 2",
+      caption: "Pie de foto Imagen 2",
+    },
+    {
+      src: "img/paisaje-3.jpg",
+      alt: "Imagen 3",
+      caption: "Pie de foto Imagen 3",
+    },
+    {
+      src: "img/paisaje-4.jpg",
+      alt: "Imagen 4",
+      caption: "Pie de foto Imagen 4",
+    },
+  ]);
+  const [url, setUrl] = useState('');
+  const [caption, setCaption] = useState('');
+
+  const agregarImagen = () =>
+  {
+    if (url && caption)
+    {
+      setImagenes([...imagenes, { src: url, alt: `imagen ${imagenes.length + 1}`, caption }]);
+      setUrl('');
+      setCaption('');
+    } else
+    {
+      alert('Por favor introduce imagen y pie PELOTUDO')
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: 'center' }}>GALERIA DE IMAGENES</h1>
+      <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+        <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} placeholder='URL de la imagen' />
+        <input type="text" value={caption} onChange={(e) => setCaption(e.target.value)} placeholder='Pie de foto' />
+        <button onClick={agregarImagen}>Agregar Imagen</button>
+      </div>
+
+      <div className='galeria' style={{ display: 'flex', gap: '10px', justifyContent: 'center', margin: '0 auto', maxWidth: '80%' }}>
+        {imagenes.map((img, index) => (
+          <div key={index}>
+            <img src={img.src} alt={img.alt} width="300" height="200" />
+            <p>{img.caption}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default GaleriaDeImagenes;
